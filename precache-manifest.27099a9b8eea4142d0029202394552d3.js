7ce15fcab17d828d82005b0307fd259e752e29b2
IlyaBondar <prince.dp.u@gmail.com>
2020-09-02 00:12:27 +0300

    Updates

---
+++ precache-manifest.27099a9b8eea4142d0029202394552d3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b8f17eb8a96ad2a40a0c0e172ad3617",
+    "url": "/Resume/index.html"
+  },
+  {
+    "revision": "de5952805c6a7522b295",
+    "url": "/Resume/static/css/main.ea0f6d85.chunk.css"
+  },
+  {
+    "revision": "5dc4429a05355380969d",
+    "url": "/Resume/static/js/2.18b8025c.chunk.js"
+  },
+  {
+    "revision": "54fd6816419f7824e2804745930c9117",
+    "url": "/Resume/static/js/2.18b8025c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de5952805c6a7522b295",
+    "url": "/Resume/static/js/main.a52ac326.chunk.js"
+  },
+  {
+    "revision": "f1901266f684724560d5",
+    "url": "/Resume/static/js/runtime-main.6aaeef75.js"
+  },
+  {
+    "revision": "38ef1c6927041c8ededff2397258e37c",
+    "url": "/Resume/static/media/logo.38ef1c69.png"
+  }
+]);
